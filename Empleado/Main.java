a3ef5cdde024f96c41a7f562f9ea4bab9278a88d
wendyRodri <68307276+wendyRodri@users.noreply.github.com>
2020-07-28 21:32:46 -0500

    Update Main.java

--- Empleado/Main.java
+++ Empleado/Main.java
@@ -12,22 +12,25 @@ public class Main {
     public static void main(String[] args) {
 	Menu m = new Menu();
                 
-    int numero = 0;
-        while(numero != 5) {
-            numero = Integer.parseInt(JOptionPane.showInputDialog("MENU" + "\n" + "1: Agregar Empleado"
-                + "\n" + "2: Eliminar empleado" + "\n" + "3: Modificar Empleado" + "\n" + "4: Mostrar Empleados" + "\n" + "5: Salir"));
+    int numero = -1;
+    
+    while(numero != 0) {
+       numero = Integer.parseInt(JOptionPane.showInputDialog("MENU" + "\n" + "1: Agregar Empleado"
+           + "\n" + "2: Eliminar Empleado" + "\n" + "3: Modificar Empleado" + "\n" + "4: Mostrar Empleados" + "\n" + "5: Empleado con Mayor Salario"
+           + "\n" + "6: Empleado con Menor Salario" + "\n" + "7: Empleados Ordenados por Nombre" + "\n" + "8: Suma de Salarios Mayores a 700" + "\n" 
+           + "9: Empleados con Apellido que inician en A " + "\n" + "10: Cinco Empleados con Mayor Salario" + "\n" + "0: SALIR" ));
             
     switch (numero) {
         case 1:
             try {
                 
-                int id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Id del Empleado"));
-                    String nombre = JOptionPane.showInputDialog("Ingrese Nombre del Empleado");
-                    String apellido = JOptionPane.showInputDialog("Ingrese Apellido del Empleado");
-                    double salario = Double.parseDouble(JOptionPane.showInputDialog("Ingrese Salario del Empleado"));
+                int id = Integer.parseInt(JOptionPane.showInputDialog("INGRESE IDENTIFICACIÒN DEL EMPLEADO"));
+                    String nombre = JOptionPane.showInputDialog("INGRESE NOMBRE DEL EMPLEADO");
+                    String apellido = JOptionPane.showInputDialog("INGRESE APELLIDO DEL EMPLEADO");
+                    double salario = Double.parseDouble(JOptionPane.showInputDialog("INGRESE SALARIO DEL EMPLEADO"));
 					
                     if(m.agregar(id, nombre, apellido, salario)) {
-                        JOptionPane.showMessageDialog(null, "Agregó");
+                        JOptionPane.showMessageDialog(null, "Se Agrego Correctamente");
                     } break;
             }
             catch (Exception e) {
@@ -36,10 +39,10 @@ public class Main {
                         
         case 2:
             try {
-                int id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Id del Empleado"));
+                int id = Integer.parseInt(JOptionPane.showInputDialog("INGRESE ID DEL EMPLEADO"));
 		
                 if (m.Eliminar(id)) {
-                    JOptionPane.showMessageDialog(null, "Eliminó");
+                    JOptionPane.showMessageDialog(null, " Se Eliminó Correctamente");
 		}break;
             }
             catch (Exception e) {
@@ -48,13 +51,13 @@ public class Main {
 				
         case 3:
             try {
-                int id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Id del Empleado"));
-                    String nombre = JOptionPane.showInputDialog("Ingrese Nombre del Empleado");
-                    String apellido = JOptionPane.showInputDialog("Ingrese Apellido del Empleado");
-                    double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite salario"));
+                int id = Integer.parseInt(JOptionPane.showInputDialog("INGRESE ID DEL EMPLEADO"));
+                    String nombre = JOptionPane.showInputDialog("INGRESE NOMBRE DEL EMPLEADO");
+                    String apellido = JOptionPane.showInputDialog("INGRESE APELLIDO DEL EMPLEADO");
+                    double salario = Double.parseDouble(JOptionPane.showInputDialog("INGRESE SALARIO DEL EMPLEADO"));
 					
                     if(m.Modificar(id, nombre, apellido, salario)) {
-                        JOptionPane.showMessageDialog(null, "Modificó");
+                        JOptionPane.showMessageDialog(null, "Se Modificó Correctamente");
                     }break;
             }
             catch (Exception e) {
@@ -71,9 +74,47 @@ public class Main {
             }break;
 				
 	case 5:
-            break;
+            try {
+                JOptionPane.showMessageDialog(null, m.EmpleadoMayorSalario());
+            } catch (Exception e) {
+              } break;
+              
+        case 6:
+            try {
+                JOptionPane.showMessageDialog(null, m.EmpleadoMenorSalario());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR");
+            }break;
+            
+        case 7:
+            try {
+                JOptionPane.showMessageDialog(null, m.EmpleadosPorNombre());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR");
+            }break;
+            
+        case 8: 
+            try {
+                JOptionPane.showMessageDialog(null, m.SumaSalarioMayor());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR");
+            }break;
+            
+        case 9:
+            try {
+                JOptionPane.showMessageDialog(null, m.TotalDeApellidosPorA());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERRORl");
+            }break;
+            
+        case 10:
+            try {
+                JOptionPane.showMessageDialog(null, m.cincoEmpleadosMayorSalario());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR");
+            }
+            
     }
+        }
 	}
-
-    }
 }
