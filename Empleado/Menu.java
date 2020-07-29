e37a29c3c8935601c795419bd4157bc9936bcb85
wendyRodri <68307276+wendyRodri@users.noreply.github.com>
2020-07-28 21:37:57 -0500

    Update Menu.java

--- Empleado/Menu.java
+++ Empleado/Menu.java
@@ -13,6 +13,14 @@ public class Menu {
 
 	public Menu() {
             lista = new ArrayList<Empleado>();
+            
+            lista = new ArrayList<Empleado>();
+            lista.add(new Empleado(1,"Wendy", "Rodriguez", 1000000));
+            lista.add(new Empleado(2, "Lussiana", "Carrero", 1300000));
+            lista.add(new Empleado(3,"Shaiel", "Avila",  600000));
+            lista.add(new Empleado(4,"Alana", "Fosenca",  950000));
+            lista.add(new Empleado(5,"Sebastian", "Alarcon",  1500000));
+            lista.add(new Empleado(6,"Andres", "Suescun",  200000));
 	}
         
         public ArrayList<Empleado> darLista(){
@@ -59,6 +67,59 @@ public class Menu {
                     throw new Exception("ERROR, El empleado no existe");
 		}
 	}
+        
+        public String EmpleadoMayorSalario() {
+            return lista.stream()
+                .max(Comparator.comparing(Empleado::getSalario))
+                .map(Empleado::toString)
+                .orElse("ERROR");
+	}
+        
+        public String EmpleadoMenorSalario() {
+            return lista.stream()
+                .min(Comparator.comparing(Empleado::getSalario))
+                .map(Empleado::toString)
+                .orElse("ERROR");
+	}
+        
+        public String EmpleadosPorNombre() {
+            String res = "";
+		res = lista.stream()
+                        .sorted(Comparator.comparing(Empleado::getNombre))
+                        .map(Empleado::toString)
+                        .reduce(" ", (a,b) -> {
+                            return a+"\n"+b; 
+        });
+                return res;
+	}
+        
+        public double SumaSalarioMayor() {
+            return lista.stream().filter((lista) -> {
+                return lista.getSalario() > 700000;
+            })
+                    .map(Empleado::getSalario)
+                    .reduce(0.0, (a,b) ->{
+                        return a+b;
+                    });
+	}
+        
+        public long TotalDeApellidosPorA() {
+            return lista.stream()
+                    .filter(empleado -> empleado.getApellido().toUpperCase().startsWith("A"))
+                    .count();
+		
+	}
+        
+        public String cincoEmpleadosMayorSalario() {
+            return lista.stream()
+                    .sorted(Comparator.comparing(Empleado::getSalario).reversed())
+                    .map(Empleado::toString)
+                    .limit(5)
+                    .reduce(" ", (a,b) -> {
+                        return a+"\n"+b; 
+                    });
+	}
+        
         
         public String Mostrar() {
             String res  = "";
